b33906334d4a7b8b238ee8deba3096fb34afe47b
wortex213433 <omeroktensen@gmail.com>
2025-10-04 18:27:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('cookie-session');
+
 const shopRoutes = require('./routes/shop');
 const cartRoutes = require('./routes/cart');
 const checkoutRoutes = require('./routes/checkout');
 const settingsRoutes = require('./routes/settings');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// Connect MongoDB
+app.use(session({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET || 'secret'],
+  maxAge: 24*60*60*1000
+}));
+
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err));
+  .then(()=>console.log("MongoDB connected"))
+  .catch(err=>console.log(err));
 
 // Routes
 app.use(shopRoutes);
@@ -24,4 +33,8 @@ app.use(cartRoutes);
 app.use(checkoutRoutes);
 app.use(settingsRoutes);
 
-app.listen(3000, ()=>console.log("Server running on port 3000"));
+// 404
+app.use((req,res)=>res.status(404).send("Page not found"));
+
+app.listen(3000, ()=>console.log("Server running on http://localhost:3000"));
+
